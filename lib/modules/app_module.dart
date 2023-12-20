e00311c9458d2fcbb0a9a6f54e91d9099838198d
ErnandoM <ernandomanoel.freire@gmail.com>
2023-12-20 17:30:00 -0300

    adicionando o modulo order

--- lib/modules/app_module.dart
+++ lib/modules/app_module.dart
@@ -1,13 +1,15 @@
 import 'package:delivery_project_using_clean_code/modules/auth/auth_module.dart';
+import 'package:delivery_project_using_clean_code/modules/order/order_module.dart';
 import 'package:delivery_project_using_clean_code/modules/product/barrels/product_module_barrel.dart';
 import 'package:delivery_project_using_clean_code/modules/product/product_module.dart';
 
 class AppModule extends Module {
   @override
-  List<Module> get imports => [AuthModule(), ProductModule()];
+  List<Module> get imports => [AuthModule(), ProductModule(), OrderModule()];
   @override
   List<ModularRoute> get routes => [
         Route.module("/auth", module: AuthModule()),
-        Route.module("/products", module: ProductModule())
+        Route.module("/products", module: ProductModule()),
+        Route.module("/order", module: OrderModule())
       ];
 }
