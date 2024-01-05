2e0e3a18c072ce0eff29c6536c4edf118a3a60bb
ErnandoM <ernandomanoel.freire@gmail.com>
2024-01-05 11:03:03 -0300

    corrigindo a validação

--- lib/modules/auth/domain/usecase/sign_up.dart
+++ lib/modules/auth/domain/usecase/sign_up.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:delivery_project_using_clean_code/modules/auth/domain/dto/user_dto.dart';
 import 'package:delivery_project_using_clean_code/modules/auth/domain/entities/user_entity.dart';
 import 'package:delivery_project_using_clean_code/modules/auth/domain/errors/i_auth_exception.dart';
