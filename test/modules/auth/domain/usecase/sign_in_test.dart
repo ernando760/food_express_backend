516ca5171c9c88d6a1513aabdb4336377c19838d
ErnandoM <ernandomanoel.freire@gmail.com>
2024-01-05 11:10:30 -0300

    corrigindo os testes do [sign_in_test.dart] e [sign_up_test.dart]

--- test/modules/auth/domain/usecase/sign_in_test.dart
+++ test/modules/auth/domain/usecase/sign_in_test.dart
@@ -1,6 +1,7 @@
 import 'package:delivery_project_using_clean_code/modules/auth/domain/dto/user_dto.dart';
 import 'package:delivery_project_using_clean_code/modules/auth/domain/entities/user_entity.dart';
 import 'package:delivery_project_using_clean_code/modules/auth/domain/errors/i_auth_exception.dart';
+import 'package:delivery_project_using_clean_code/modules/auth/domain/repositories/i_auth_repository.dart';
 import 'package:delivery_project_using_clean_code/modules/auth/domain/usecase/sign_in.dart';
 import 'package:delivery_project_using_clean_code/modules/auth/domain/vo/vo_email.dart';
 import 'package:delivery_project_using_clean_code/modules/auth/domain/vo/vo_password.dart';
@@ -8,46 +9,54 @@ import 'package:delivery_project_using_clean_code/modules/auth/domain/vo/vo_text
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-class MockSignIn extends Mock implements SignIn {}
+class MockAuthRepository extends Mock implements IAuthRepository {}
 
 class FakeUserDto extends Fake implements UserDto {}
 
 void main() {
-  final usecase = MockSignIn();
+  late final MockAuthRepository repository;
+  late final SignIn usecase;
 
   setUpAll(() {
+    repository = MockAuthRepository();
+    usecase = SignIn(repository);
     registerFallbackValue(FakeUserDto());
   });
-  test('should make sign in and validate its values retuning true', () async {
-    when(() => usecase.call(any())).thenAnswer((_) async => (
+
+  tearDownAll(() {
+    reset(repository);
+  });
+
+  test('Should make sign in and validate its values retuning true', () async {
+    when(() => repository.signIn(any())).thenAnswer((_) async => (
           user: UserEntity(
-              username: VOText("username"),
-              email: VOEmail("email@gmail.com"),
-              password: VOPassword("password123")),
+              id: "wdwd-dadadwdw",
+              username: VOText("fulano"),
+              email: VOEmail("fulano@fulano.com"),
+              password: VOPassword("fulano123")),
           token: "token",
           exception: null
         ));
 
     final (:user, :token, :exception) = await usecase.call(UserDto(
-        username: VOText("username"),
-        email: VOEmail("email@gmail.com"),
-        password: VOPassword("password123")));
+        username: VOText("fulano"),
+        email: VOEmail("fulano@fulano.com"),
+        password: VOPassword("fulano123")));
 
     expect(user?.validate(), isTrue);
     expect(token, "token");
     expect(exception, isNull);
 
-    verify(
-      () => usecase.call(any()),
-    ).called(1);
+    verify(() => repository.signIn(any())).called(1);
   });
 
   test('should make sign in and validate its values retuning false', () async {
-    when(() => usecase.call(any())).thenAnswer((_) async => (
+    when(() => repository.signIn(any())).thenAnswer((_) async => (
           user: null,
           token: "",
           exception: InvalidSignInException(
-              messageError: "O login está invalido", label: "MockSignInUseCase")
+              messageError: "O email/senha está invalido",
+              label: "MockSignInUseCase")
         ));
 
     final (:user, :token, :exception) = await usecase.call(UserDto(
@@ -56,10 +65,7 @@ void main() {
         password: VOPassword("password123")));
     expect(user, isNull);
     expect(token, "");
-    expect(exception?.messageError, "O login está invalido");
-
-    verify(
-      () => usecase.call(any()),
-    ).called(1);
+    expect(exception?.messageError, "O email/senha está invalido");
+    // verify(() => repository.signIn(any())).called(1);
   });
 }
--- test/modules/auth/domain/usecase/sign_up_test.dart
+++ test/modules/auth/domain/usecase/sign_up_test.dart
@@ -1,6 +1,7 @@
 import 'package:delivery_project_using_clean_code/modules/auth/domain/dto/user_dto.dart';
 import 'package:delivery_project_using_clean_code/modules/auth/domain/entities/user_entity.dart';
 import 'package:delivery_project_using_clean_code/modules/auth/domain/errors/i_auth_exception.dart';
+import 'package:delivery_project_using_clean_code/modules/auth/domain/repositories/i_auth_repository.dart';
 import 'package:delivery_project_using_clean_code/modules/auth/domain/usecase/sign_up.dart';
 import 'package:delivery_project_using_clean_code/modules/auth/domain/vo/vo_email.dart';
 import 'package:delivery_project_using_clean_code/modules/auth/domain/vo/vo_password.dart';
@@ -8,58 +9,63 @@ import 'package:delivery_project_using_clean_code/modules/auth/domain/vo/vo_text
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-class MockSignUp extends Mock implements SignUp {}
+class MockAuthRepository extends Mock implements IAuthRepository {}
 
 class FakeUserDto extends Fake implements UserDto {}
 
 void main() {
-  final usecase = MockSignUp();
+  late final IAuthRepository repository;
+  late final SignUp usecase;
 
   setUpAll(() {
+    repository = MockAuthRepository();
+    usecase = SignUp(repository);
     registerFallbackValue(FakeUserDto());
   });
+
+  tearDownAll(() {
+    reset(repository);
+  });
+
   test('should make sign up and validate its values retuning true', () async {
-    when(() => usecase.call(any())).thenAnswer((_) async => (
+    when(() => repository.signUp(any())).thenAnswer((_) async => (
           user: UserEntity(
-              username: VOText("username"),
-              email: VOEmail("email@gmail.com"),
-              password: VOPassword("password123")),
+              id: "dwwdw-adaddwdw",
+              username: VOText("fulano"),
+              email: VOEmail("fulano@fulano.com"),
+              password: VOPassword("fulano123")),
           token: 'token',
           exception: null
         ));
 
     final (:user, :token, :exception) = await usecase.call(UserDto(
-        username: VOText("username"),
-        email: VOEmail("email@gmail.com"),
-        password: VOPassword("password123")));
+        username: VOText("fulano"),
+        email: VOEmail("fulano@fulano.com"),
+        password: VOPassword("fulano123")));
 
     expect(user?.validate(), isTrue);
     expect(token, "token");
     expect(exception, isNull);
 
-    verify(
-      () => usecase.call(any()),
-    ).called(1);
+    verify(() => repository.signUp(any())).called(1);
   });
 
   test('should make sign up and validate its values retuning false', () async {
-    when(() => usecase.call(any())).thenAnswer((_) async => (
+    when(() => repository.signUp(any())).thenAnswer((_) async => (
           user: null,
           token: '',
           exception: InvalidSignUpException(
-              messageError: "O login está invalido", label: "MockSignUp")
+              messageError: "O email/senha está invalido", label: "label")
         ));
 
     final (:user, :token, :exception) = await usecase.call(UserDto(
         username: VOText(""),
         email: VOEmail("email.com"),
-        password: VOPassword("password123")));
+        password: VOPassword("fualano123")));
     expect(user, isNull);
     expect(token, "");
-    expect(exception?.messageError, "O login está invalido");
+    expect(exception?.messageError, "O email/senha está invalido");
 
-    verify(
-      () => usecase.call(any()),
-    ).called(1);
+    verify(() => repository.signUp(any())).called(1);
   });
 }
